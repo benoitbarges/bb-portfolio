839554ac00a95507745cba3c0442d82cd320efe5
benoit barges <benoitbarges@gmail.com>
2020-09-15 14:24:00 +0200

    fixed again hande animate js for max screen

--- app/javascript/components/handle_animate.js
+++ app/javascript/components/handle_animate.js
@@ -1,12 +1,12 @@
 const handleAnimate = () => {
   const names = document.querySelectorAll(".project-name");
-  if (window.matchMedia("(max-width: 780px)").matches) {
-    names.forEach((name) => {
-      name.addEventListener('animationend', () => {
+  names.forEach((name) => {
+    name.addEventListener('animationend', () => {
+      if (window.matchMedia("(max-width: 780px)").matches) {
         name.style.position = "fixed";
-      });
+      }
     });
-  }
+  });
 };
 
 export { handleAnimate };
