6ab28e282ec1f934bc268ca3f41e64bfba5600a9
benoit barges <benoitbarges@gmail.com>
2020-09-15 13:01:07 +0200

    wroking in reponsive

--- app/javascript/components/open_project.js
+++ app/javascript/components/open_project.js
@@ -10,17 +10,11 @@ const openProject = () => {
       projectName.addEventListener('click', (event) => {
         projectName.classList.add('animate__fadeOutRight');
         projectElement.classList.add('slide-right');
-        if (!projectCard.querySelector(".multi-collapse").classList.contains("show")) {
-          projectDiv.nextElementSibling.scrollIntoView({behavior: "smooth", block: "center"})
-        }
       });
 
       projectElement.addEventListener('click', (event) => {
         projectName.classList.add('animate__fadeOutRight');
         projectElement.classList.add('slide-right');
-        if (!projectCard.querySelector(".multi-collapse").classList.contains("show")) {
-          projectDiv.nextElementSibling.scrollIntoView({behavior: "smooth", block: "center"})
-        }
       });
     }
 
@@ -28,16 +22,11 @@ const openProject = () => {
       projectName.addEventListener('click', (event) => {
         projectName.classList.add('animate__fadeOutLeft');
         projectElement.classList.add('slide-left');
-        if (!projectCard.querySelector(".multi-collapse").classList.contains("show")) {
-          projectDiv.nextElementSibling.scrollIntoView({behavior: "smooth", block: "center"})
-        }
       });
        projectElement.addEventListener('click', (event) => {
         projectName.classList.add('animate__fadeOutLeft');
         projectElement.classList.add('slide-left');
-         if (!projectCard.querySelector(".multi-collapse").classList.contains("show")) {
-          projectDiv.nextElementSibling.scrollIntoView({behavior: "smooth", block: "center"})
-        }
+
       });
     }
   });
