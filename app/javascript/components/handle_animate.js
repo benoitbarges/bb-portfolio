f208cfabdaa14764440a4a1a72d63bfc9814114f
benoit barges <benoitbarges@gmail.com>
2020-09-15 14:18:31 +0200

    fixed hande animate js for max screen

--- app/javascript/components/handle_animate.js
+++ app/javascript/components/handle_animate.js
@@ -1,10 +1,12 @@
 const handleAnimate = () => {
   const names = document.querySelectorAll(".project-name");
-  names.forEach((name) => {
-    name.addEventListener('animationend', () => {
-      name.style.position = "fixed";
+  if (window.matchMedia("(max-width: 780px)").matches) {
+    names.forEach((name) => {
+      name.addEventListener('animationend', () => {
+        name.style.position = "fixed";
+      });
     });
-  });
+  }
 };
 
 export { handleAnimate };
